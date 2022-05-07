fd53a327dddf77aaa38a061a83d7d0a3e3978eee
Adib Zaini <muhammadadibzaini@gmail.com>
2022-05-07 14:21:56 +0800

    Add config to detect type of NODE_ENV

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,5 +1,11 @@
 export const SERVER_DOMAIN = window.location.hostname;
-export const PORT = '5000';
+
+export let PORT;
+if (process.env.NODE_ENV === 'production') {
+  PORT = '';
+} else {
+  PORT = '5000';
+}
 
 export const API_SERVER = `http://${SERVER_DOMAIN}:${PORT}`;
 export const WEB_SOCKET_SERVER = `ws://${SERVER_DOMAIN}:${PORT}`;
