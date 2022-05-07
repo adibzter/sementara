42905a5282ab4b040969eb9a49065e6d921559bb
Adib Zaini <muhammadadibzaini@gmail.com>
2022-05-07 14:32:07 +0800

    Add config for web socket protocol

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,11 +1,14 @@
 export const SERVER_DOMAIN = window.location.hostname;
 
 export let PORT;
+let wsProtocol;
 if (process.env.NODE_ENV === 'production') {
   PORT = '';
+  wsProtocol = 'wss';
 } else {
   PORT = '5000';
+  wsProtocol = 'ws';
 }
 
-export const API_SERVER = `http://${SERVER_DOMAIN}:${PORT}`;
-export const WEB_SOCKET_SERVER = `ws://${SERVER_DOMAIN}:${PORT}`;
+export const API_SERVER = window.location.origin;
+export const WEB_SOCKET_SERVER = `${wsProtocol}://${SERVER_DOMAIN}:${PORT}`;
